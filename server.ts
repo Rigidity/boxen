73443608b5a342a380163b560a17acd058734c39
Rigidity <me@rigidnetwork.com>
2026-01-12 00:14:33 -0500

    SSL support

--- server.ts
+++ server.ts
@@ -1,5 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
+import fs from "fs";
+import http from "http";
+import https from "https";
 import ViteExpress from "vite-express";
+
+dotenv.config();
+
+const PORT = process.env.PORT ?? "3000";
+const SSL_PORT = process.env.SSL_PORT;
+const SSL_KEY = process.env.SSL_KEY;
+const SSL_CERT = process.env.SSL_CERT;
 
 const games: Record<string, string> = {};
 
@@ -28,4 +39,25 @@ app.post("/set_game", (req, res) => {
   });
 });
 
-ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
+const httpServer = http.createServer(app);
+
+httpServer.listen(parseInt(PORT), "0.0.0.0", () => {
+  console.log(`HTTP server is listening on port ${PORT}`);
+});
+
+if (SSL_PORT && SSL_KEY && SSL_CERT) {
+  const options = {
+    key: fs.readFileSync(SSL_KEY),
+    cert: fs.readFileSync(SSL_CERT),
+  };
+
+  const httpsServer = https.createServer(options, app);
+
+  httpsServer.listen(parseInt(SSL_PORT), "0.0.0.0", () => {
+    console.log(`HTTPS server is listening on port ${SSL_PORT}`);
+  });
+
+  ViteExpress.bind(app, httpsServer);
+} else {
+  ViteExpress.bind(app, httpServer);
+}
